8974d951c79b007d3d808591167960b2257f14c0
Mheligne08 <mhel_021199@binalatongan.edu.ph>
2024-07-01 10:19:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -28,20 +28,12 @@ function addOrder() {
         var order3 = qty3.value.toString() + " pcs x " + product3.textContent + " - Php " + (parseFloat(qty3.value) * parseFloat(price3.textContent)).toFixed(2) + "\n";
         carts.textContent += order3;
     }
-    if (parseFloat(qty4.value) > 0) {
-        var order4 = qty4.value.toString() + " pcs x " + product4.textContent + " - Php " + (parseFloat(qty4.value) * parseFloat(price4.textContent)).toFixed(2) + "\n";
-        carts.textContent += order4;
-    }
-    if (parseFloat(qty5.value) > 0) {
-        var order5 = qty5.value.toString() + " pcs x " + product5.textContent + " - Php " + (parseFloat(qty5.value) * parseFloat(price5.textContent)).toFixed(2) + "\n";
-        carts.textContent += order5;
-    }
-    if (parseFloat(qty6.value) > 0) {
-        var order6 = qty6.value.toString() + " pcs x " + product6.textContent + " - Php " + (parseFloat(qty6.value) * parseFloat(price6.textContent)).toFixed(2) + "\n";
-        carts.textContent += order6;
-    }
-    updateTotal(); // Update total after adding orders
+
+    // Call updateTotal after adding items to the cart
+    updateTotal();
 }
+
+
 
 function updateTotal() {
     var total = 0;
